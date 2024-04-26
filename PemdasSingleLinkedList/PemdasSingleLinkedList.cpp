b3c021ecb216413cd0401ee25623b77965904a3c
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-04-26 11:21:33 +0700

    Menghapus node dengan NIM tertentu dari daftar

--- PemdasSingleLinkedList/PemdasSingleLinkedList.cpp
+++ PemdasSingleLinkedList/PemdasSingleLinkedList.cpp
@@ -51,7 +51,7 @@ void addNode() {
 
 }
 
-bool searchNode(int nim, Node* current, Node* previous) {
+bool serachNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
 	while (current != NULL && nim > current->noMhs)
@@ -74,4 +74,12 @@ bool searchNode(int nim, Node* current, Node* previous) {
 	}
 }
 bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (serachNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
 }
